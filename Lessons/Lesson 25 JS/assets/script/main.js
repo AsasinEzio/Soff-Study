8641bdacdf51bc97feb3814b40d887a3d2f66cb6
John Sodiqov <jahongirsodiqov2@gmail.com>
2023-01-28 20:15:49 +0500

    First js project

---
+++ Lessons/Lesson 25 JS/assets/script/main.js
@@ -0,0 +1,20 @@
+// let student = {
+//     name: "Falonchi",
+//     surname: "Falonchiev",
+//     age: 30,
+//     study: "TATU"
+// }
+// console.log(student);
+
+// let meni_ismim = "John"
+
+// console.log(meni_ismim);
+
+
+// let a, b;
+// a = +prompt("A ni kiriting");
+// b = +prompt("B ni kiriting");
+
+// console.log(a ** b);
+
+
