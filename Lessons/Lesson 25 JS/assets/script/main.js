02d596bf9d6487ef54b1b04c7dd83be5ded42868
John Sodiqov <jahongirsodiqov2@gmail.com>
2023-01-31 17:50:18 +0500

    JS change

--- Lessons/Lesson 25 JS/assets/script/main.js
+++ Lessons/Lesson 25 JS/assets/script/main.js
@@ -1,20 +1,57 @@
-// let student = {
-//     name: "Falonchi",
-//     surname: "Falonchiev",
-//     age: 30,
-//     study: "TATU"
-// }
-// console.log(student);
+// 1-mashq
 
-// let meni_ismim = "John"
+// Nolga teng bo'lmagan ikta son berilgan. Ularning yig'indisini, ko'paytmasini, va har birining kvadrati aniqlansin 
 
-// console.log(meni_ismim);
+// let a = +prompt("A ni kiriting"), b = +prompt("B ni kiriting");
+
+// console.log( "A va B ning yig'indisi=" + (a + b));
+// console.log( "A va B ning ayirmasi=" + (a - b));
+// console.log( "A va B ning ko'paytmasi=" + (a * b));
+// console.log( "A va B ning bo'linmasi=" + (a / b));
+
+// 2-mashq
+
+// A, B va C sonlari berilgan. A ni qiymati B ga, B ni qiymati C ga, C ni qiymati A ga almashtirilsin. A, B va C sonlarining yangi qiymatlari ekranga chiqarilsin.
+
+// let a,
+//     b,
+//     c,
+//     d,
+//     e;
+// a = 1;
+// b = 2;
+// c = 3;
+
+// console.log(a, b, c);
+
+// d = a;
+// e = b;
+// a = c;
+// b = d;
+// c = e;
+
+// console.log(a, b, c);
+
+//3-mashq
+
+// Og'irlik X kilogramda berilgan. Undagi to'liq tonnalar sonini aniqlovchi programma tuzilsin.
+
+// let x=+prompt("Kilogrammni kiriting"),t=1000;
+
+// console.log("X " + (x/t) +" tonnaga teng");
+
+// 4-mashq
+
+// Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha soat va sekund o'tganini aniqlovchi programma tuzilsin.
+
+// let N = +prompt("Daqiqani kiriting");
+// let s, c;
+// s = N / 60;
+// c = N % 60;
+// console.log("Kun boshlanganidan " + Math.round(s) + ` soat ${c} daqiqa o'tdi`);
 
 
-// let a, b;
-// a = +prompt("A ni kiriting");
-// b = +prompt("B ni kiriting");
 
-// console.log(a ** b);
+
 
 
