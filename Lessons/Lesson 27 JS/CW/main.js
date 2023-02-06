e6e4eeaddd559dac24fa0815a2d13adea9162001
John Sodiqov <jahongirsodiqov2@gmail.com>
2023-02-06 15:20:17 +0500

    Js change

---
+++ Lessons/Lesson 27 JS/CW/main.js
@@ -0,0 +1,90 @@
+// let a = +prompt("Istalgan son kiriting");
+
+// if (a == 0){
+//     a=10;
+//     alert(a)
+// }else if(a>0){
+//     ++a;
+//     alert(a)
+// }else{
+//     a-=2;
+//     alert(a)
+// }
+
+// let a = +prompt("Birinchi sonni kiriting")
+// let b = +prompt("Ikkinchi sonni kiriting")
+// let c = +prompt("Ikkinchi sonni kiriting")
+
+// if (a !== b){
+//     alert(a+b)
+// }else {
+//     alert(0)
+// }
+
+// if (a<b){
+//     alert(1);
+// }else if(a>b){
+//     alert(2);
+// }else{
+//     alert("Bu sonlar o'zaro teng");
+// }
+
+// if (a !== b) {
+//     alert(Math.max(a, b))
+// } else{
+//     alert(0)
+// }
+
+// if (a < b && b < c) {
+//     a *= 2;
+//     b *= 2;
+//     c *= 2;
+//     alert(a + " " + b + " " + c);
+// } else {
+//     a -= 5;
+//     b -= 5;
+//     c -= 5;
+//     alert(a + " " + b + " " + c);
+// }
+
+// if ((a<b && b<c) || (c<b && b<a)){
+//     alert(b);
+// }else if((a<c && c<b) || (b<c && c<a)){
+//     alert(c);
+// }else if((b<a && a<c) || (c<a && a<b)){
+//     alert(a);
+// }else {
+//     alert(0)
+// }
+
+// let x = +prompt("X ni kiriting");
+// switch(x){
+//     case 1: alert("Yanvar"); break;
+//     case 2: alert("Fevral"); break;
+//     case 3: alert("Mart"); break;
+//     case 4: alert("Aprel"); break;
+//     case 5: alert("May"); break;
+//     case 6: alert("Iyun"); break;
+//     case 7: alert("Iyul"); break;
+//     case 8: alert("Avgust"); break;
+//     case 9: alert("Senytabr"); break;
+//     case 10: alert("Oktyabr"); break;
+//     case 11: alert("Noyabr"); break;
+//     case 12: alert("Dekabr"); break;
+//     default: alert("Bir yilda 12 oy bor")
+// }
+// let a;
+// for (a=0; a<100; a+=2){
+//     console.log(a)
+// }
+
+// let a = 10
+// let b = 20
+// let c = 0
+
+// for (let i = a; i < b; i++) {
+//     alert(c = c + i);
+// }
+
+
+
---
+++ Lessons/Lesson 27 JS/HW/main.js
@@ -0,0 +1,28 @@
+// 1 - mashq 
+
+// let a = +prompt("Ikki xonali son kiritng")
+// console.log("O'nlik xonasidagi son " + parseInt(a/10));
+// console.log("Birlik xonasidagi son " + a%10);
+// console.log("Ikkala sonning yig'indisi " + ((a%10) + parseInt(a/10)))
+
+// 2 - mashq
+
+// let a = +prompt("Ikki xonali son kiritng");
+// let b = (String(a%10) + parseInt(a/10));
+// console.log(Number(b));
+
+// 3 - mashq 
+
+// let a = +prompt("A kesmaning uzunligini kiriting")
+// let b = +prompt("B kesmaning uzunligini kiriting")
+
+// console.log((a/b).toFixed(1));
+
+// // 4 - mashq 
+
+// let a = +prompt("Ikki xonali son kiritng");
+
+// console.log(parseInt(a/1000));
+// console.log(parseInt(a/));
+// console.log(parseInt(a/100000));
+// console.log(parseInt(a/1000000));
