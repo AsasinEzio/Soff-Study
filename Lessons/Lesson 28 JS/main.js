9acc1ec5e50a88aa0d53100ee392f9dac8560e38
John Sodiqov <jahongirsodiqov2@gmail.com>
2023-02-07 19:24:06 +0500

    JS change

---
+++ Lessons/Lesson 28 JS/main.js
@@ -0,0 +1,53 @@
+// 4 - mashq
+// let a = +prompt("Sonni kiriting");
+
+
+// for (i = 1; i < a; i++) {
+//     let y = 0
+//     for (j = 0; j < i; j++) {
+//         if (i % j == 0) {
+//             y += j
+//         }
+//     }
+//     if (y == i) {
+//         console.log(i);
+//     }
+// }
+
+// 5 - mashq
+
+// let N = +prompt("N sonini kiriting")
+
+// for (i = 0; i <= N; i++) {
+//     if ((i % 3 == 0) && (i % 5 != 0)){
+//         console.log(i);
+//     }
+// }
+
+// 6 - mashq
+
+// let a = +prompt("Son kiriting")
+
+// if (a == 1){
+//     console.log("1 tub ham natural ham emas");
+// }else if(((a%1 == 0) && (a%a == 0)) && ((a%2 != 0) && (a%3 != 0)) ){
+//     console.log("Bu son tub son");
+// }else{
+//     console.log("Bu son tub son emas");
+// }
+//  7 - mashq
+
+// let a = +prompt("Son kiriting")
+
+// for (i = 0; i <= a; i++) {
+//     if (i == 1) {
+//         console.log("1 tub ham natural ham emas");
+//     } else if (((i % 1 == 0) && (i % i == 0)) && ((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0))) {
+//         console.log(i + " bu son tub son");
+
+//     } else if (i == 3 || i == 5) {
+//         console.log(i + " bu tub son");
+//     } else {
+//         console.log(i + " bu son tub son emas");
+//     }
+// }
