2df779b3f71d57f74ec30c3507a87c321e426406
John Sodiqov <jahongirsodiqov2@gmail.com>
2023-02-11 14:05:27 +0500

    JS Commit

---
+++ Lessons/Lesson 30 JS/main.js
@@ -0,0 +1,97 @@
+
+// 1 - mashq
+
+let users = [
+    {
+        name: "Anton",
+        age: 28,
+        number: 9989999999,
+        email: "antonchanton@gmail.com",
+        hobbies: ["Futbol", "Anime", "Koding"],
+        id: 1
+
+    },
+    {
+        name: "Kiril",
+        age: 32,
+        number: "+1212199887",
+        email: "kiriltsa@gmail.com",
+        hobbies: ["Plavanie", "Multik", "Igri"],
+        id: 2
+
+    },
+    {
+        name: "Valeriy",
+        age: 23,
+        number: "+7 898 776 543",
+        email: "valeravaxovskiy@gmail.com",
+        hobbies: ["Kino", "Spat", "Pech"],
+        id: 3
+
+    },
+    {
+        name: "Matvey",
+        age: 43,
+        number: "+7 898 454 343",
+        email: "matvienko@gmail.com",
+        hobbies: ["Chtenie", "Tennis", "Vojdenie"],
+        id: 4
+
+    },
+    {
+        name: "Ekatirina",
+        age: 21,
+        number: "+7 908 764 873",
+        email: "ekatya@gmail.com",
+        hobbies: ["Basketbol", "Shit", "Kupatsya"],
+        id: 5
+
+    },
+
+]
+// 2 - mashq
+
+// console.log(`${users[0].id} - ` + users[0].name + ` ${users[0].age} yoshda`);
+// console.log(`${users[0].id} - ` + users[0].name + ` ${users[0].age} yoshda,` + ` kontaktlari ${users[0].number}` + ` ${users[0].email}`);
+// console.log(`${users[0].id} - ` + users[0].name + ` yoqtirgan hobbilari => ${users[0].hobbies}`);
+
+// 3 - mashq
+
+// console.log(users[2].hobbies);
+// users[2].hobbies.splice(+prompt("massiv element raqami"), +prompt("O'chirmoqchi bo'lgan elementlar soni"), prompt("Yangi hobbi yozing"))
+// console.log(users[2].hobbies);
+
+// 4 - mashq
+
+// users.pop();
+// console.log(users);
+
+// 5 - mashq    
+
+// for (i = 0; i < users.length - 1; i++) {
+//     if (users[i].name == "Matvey") {
+//         users[i].name = "Timofey"
+//         users[i].age = 34
+//         users[i].number = "897897980"
+//         users[i].email = "timofey3443@mail.ru"
+//         users[i].hobbies = ["Tanki", "Panki", "Ranki"]
+//         users[i].id = `${i}`
+//     }
+// }
+
+// 6 - mashq
+
+// users.push({
+//     name: prompt("New user name"),
+//     age: +prompt("New user age"),
+//     number: +prompt("New user number"),
+//     email: prompt("New user email"),
+//     hobbies: prompt("New user hobbies"),
+//     id: +prompt("New user id")
+// })
+
+
+
+
+
+
