bfcb2ff3cb091b87c2b7a78cb8380db45a33fb84
John Sodiqov <jahongirsodiqov2@gmail.com>
2023-02-11 18:34:32 +0500

    Js Commit

--- Lessons/Lesson 30 JS/main.js
+++ Lessons/Lesson 30 JS/main.js
@@ -68,16 +68,16 @@ let users = [
 
 // 5 - mashq
 
-// for (i = 0; i < users.length - 1; i++) {
-//     if (users[i].name == "Matvey") {
-//         users[i].name = "Timofey"
-//         users[i].age = 34
-//         users[i].number = "897897980"
-//         users[i].email = "timofey3443@mail.ru"
-//         users[i].hobbies = ["Tanki", "Panki", "Ranki"]
-//         users[i].id = `${i}`
-//     }
-// }
+for (i = 0; i < users.length - 1; i++) {
+    if (users[i].name == "Matvey") {
+        users[i].name = "Timofey"
+        users[i].age = 34
+        users[i].number = "897897980"
+        users[i].email = "timofey3443@mail.ru"
+        users[i].hobbies = ["Tanki", "Panki", "Ranki"]
+        users[i].id = `${i}`
+    }
+}
 
 // 6 - mashq
 
@@ -99,7 +99,7 @@ let users = [
 
 // 9 - mashq
 
-// console.log(news[4].categories);
+// console.log(news[+prompt("Son kiriting")].categories);
 
 
 
